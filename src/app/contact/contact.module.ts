156edac4549dab349f80d5952dd04b43aca4aee5
rameshkmscct <rameshkmscct@gmail.com>
2021-12-16 00:04:39 +0530

    7 files modified, 1 added, 1 deleted

--- src/app/contact/contact.module.ts
+++ src/app/contact/contact.module.ts
@@ -1,10 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ContactComponent } from './contact.component';
 
 @NgModule({
-  imports: [
-    CommonModule
-  ],
-  declarations: []
+  imports: [CommonModule],
+  declarations: [ContactComponent],
+  exports: [ContactComponent],
 })
-export class ContactModule { }
+export class ContactModule {}
